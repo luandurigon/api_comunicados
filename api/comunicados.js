9a366c1e73f4e225b870fe6b086f3f5b6ea04e9a
luandurigon <luandurigon@gmail.com>
2026-03-30 20:18:54 -0300

    Add files via upload

--- api/comunicados.js
+++ api/comunicados.js
@@ -3,6 +3,6 @@ const lista = require ("../dados/comunicados.json");
 
 export default function handler(req, res) {
 
-    return res.satatus(200).json(...);
+    return res.satatus(200).json(lista);
 
 }
