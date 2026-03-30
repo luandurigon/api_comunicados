65f56e6c41a46862fdd36dfe4e17548e53193d3b
luandurigon <luandurigon@gmail.com>
2026-03-30 20:16:23 -0300

    Delete api directory

--- api/api/comunicados.js
+++
@@ -1,8 +0,0 @@
-const lista = require ("../dados/comunicados.json");
-
-
-export default function handler(req, res) {
-
-    return res.satatus(200).json(...);
-
-}
--- api/api/status.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        status: "ok",
-        sistema: "API de Comunicados Eventos ",
-        ambiente: "Cloud"
-    });
-}
